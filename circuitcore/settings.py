618404370826f14bb128165383a0d82a20f862be
TocinoEspacial <pasta0868@gmail.com>
2025-06-26 17:49:24 -0500

    static files

--- circuitcore/settings.py
+++ circuitcore/settings.py
@@ -90,8 +90,6 @@ TEMPLATES = [
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'ygv/static')]  # Si está dentro de tu app
-
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
@@ -159,7 +157,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
