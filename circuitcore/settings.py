ea99fd83ddacd28daacf0d54c96e550adf2b210c
TocinoEspacial <pasta0868@gmail.com>
2025-06-26 17:07:20 -0500

    cookies

--- circuitcore/settings.py
+++ circuitcore/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -89,7 +89,9 @@ TEMPLATES = [
 ]
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / 'static']
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'ygv/static')]
+
+
 
 WSGI_APPLICATION = 'circuitcore.wsgi.application'
 
