af30aed3e44fdc7ecad3b7ac7d3b23ed525efe87
TocinoEspacial <pasta0868@gmail.com>
2025-06-26 19:09:09 -0500

    super user create

--- circuitcore/settings.py
+++ circuitcore/settings.py
@@ -164,12 +164,3 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
-if os.environ.get('DJANGO_SUPERUSER_PASSWORD'):
-    from django.contrib.auth import get_user_model
-    User = get_user_model()
-    if not User.objects.filter(username=os.environ['DJANGO_SUPERUSER_USERNAME']).exists():
-        User.objects.create_superuser(
-            username=os.environ['DJANGO_SUPERUSER_USERNAME'],
-            email=os.environ['DJANGO_SUPERUSER_EMAIL'],
-            password=os.environ['DJANGO_SUPERUSER_PASSWORD']
-        )
--- create_superuser.py
+++ create_superuser.py
@@ -1,14 +1,17 @@
-# create_superuser.py
+import os
 from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
-username = "daniel"
-email = "pasta0868@gmail.com"
-password = "123456789"
+username = os.environ.get('DJANGO_SUPERUSER_USERNAME')
+email = os.environ.get('DJANGO_SUPERUSER_EMAIL')
+password = os.environ.get('DJANGO_SUPERUSER_PASSWORD')
 
-if not User.objects.filter(username=username).exists():
-    User.objects.create_superuser(username=username, email=email, password=password)
-    print("✅ Superusuario creado con éxito.")
+if username and password and email:
+    if not User.objects.filter(username=username).exists():
+        User.objects.create_superuser(username=username, email=email, password=password)
+        print("✅ Superusuario creado con éxito.")
+    else:
+        print("⚠️ El superusuario ya existe.")
 else:
-    print("⚠️ El superusuario ya existe.")
+    print("❌ Variables de entorno faltantes.")
